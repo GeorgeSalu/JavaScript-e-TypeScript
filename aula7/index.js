475c4a4c6f672fb562f2ac35b5c0ac3b85f4e8e5
George Salu <george.salu10@gmail.com>
2020-11-11 11:31:50 -0300

    constantes com const

---
+++ aula7/index.js
@@ -0,0 +1,14 @@
+// não podemos criar constantes com palavras reservadas
+// constantes precisam ter nomes significativos
+// não pode comecar o nome de uma constantes com um numero
+// não podem contaer espaços ou tracos 
+// utilizamos camelcase
+// não pode modificar o valor de uma constante
+// não utilize var, utilize const
+
+const primeiroNumero = 5
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero
+const resultadoDuplicado = resultado * 2
+
+console.log(resultadoDuplicado)

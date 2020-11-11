eae0b3ee366ba73df2a91e8f2641cb2007ffb2fc
George Salu <george.salu10@gmail.com>
2020-11-11 13:29:58 -0300

    operadores atirmeticos, de atribuição e incremento

---
+++ aula10/index.js
@@ -0,0 +1,9 @@
+// operadores atimeticos, de atribuição e incremento
+const num1 = 5;
+const num2 = 2;
+const num3 = 10;
+console.log(num1 + num2 + num3)
+
+let contador = 1
+contador++
+console.log(contador)

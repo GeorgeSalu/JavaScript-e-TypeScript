66a78ae6a394bd1e495222a01242a2eadb5a9592
George Salu <george.salu10@gmail.com>
2020-11-16 11:35:19 -0300

    criando uma lsita de tarefas - 4

--- javascript/aula48/assets/js/main.js
+++ javascript/aula48/assets/js/main.js
@@ -33,6 +33,7 @@ function criaTarefa(textoInput) {
 	tarefas.appendChild(li);
 	limpaInput();
 	criaBotaoApagar(li);
+	salvarTarefas();
 }
 
 btnTarefa.addEventListener('click', function(e) {
@@ -44,5 +45,20 @@ document.addEventListener('click', function(e) {
 	const el = e.target;
 	if(el.classList.contains('apagar')) {
 		el.parentElement.remove();
+		salvarTarefas();
 	}
 })
+
+function salvarTarefas() {
+	const liTarefas = tarefas.querySelectorAll('li');
+	const listaDeTarefas = [];
+
+	for(let tarefa of liTarefas) {
+		let tarefaTexto = tarefa.innerText;
+		tarefaTexto = tarefaTexto.replace('Apagar', '').trim();
+		listaDeTarefas.push(tarefaTexto);
+	}
+
+	const tarefasJSON = JSON.stringify(listaDeTarefas);
+	localStorage.setItem('tarefas', tarefasJSON);
+}

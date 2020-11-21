5d0476520fcf1b463362f5023fe179145bdb72d3
George Salu <george.salu10@gmail.com>
2020-11-21 15:08:14 -0300

    webpack (boilerplate)

--- javascript/aula93/webpack.config.js
+++ javascript/aula93/webpack.config.js
@@ -6,5 +6,18 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, 'public', 'assets', 'js'),
 		filename: 'bundle.js'
-	}
+	},
+	module: {
+		rules: [{
+			exclude: /node_modules/,
+			test: /\.js$/,
+			use: {
+				loader: 'babel-loader',
+				options: {
+					presets: ['@babel/env']
+				}
+			}
+		}]
+	},
+	devtool: 'source-map'
 }

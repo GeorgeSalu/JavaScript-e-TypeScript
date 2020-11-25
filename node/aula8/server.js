a66aaa95696e5ba16098955b7d8b9fde44f5074d
George Salu <george.salu10@gmail.com>
2020-11-25 10:51:05 -0300

    express - req.params, req.query e req.body

--- node/aula8/server.js
+++ node/aula8/server.js
@@ -3,22 +3,28 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 
-
 app.get('/', (req, res) => {
   res.send(`
 		<form action="/" method="POST">
-		Nome: <input type="text" name="nome">
-		<button>Enviar</button>
+		Nome do cliente: <input type="text" name="qualquercoisa"><br>
+		Outro campo: <input type="text" name="aquioutrocampo">
+		<button>Olá mundo</button>
 		</form>
   `);
 });
 
-app.post('/', (req, res) => {
-  res.send('Recebi o formulário');
+app.get('/testes/:idUsuarios?/:parametro?', (req, res) => {
+  // /profiles/3
+  // /profiles/?chave1=valor1&chave2=valor2&chave3=valor3
+  //
+  console.log(req.params);
+  console.log(req.query);
+  res.send(req.query.facebookprofile);
 });
 
-app.get('/contato', (req, res) => {
-  res.send('Obrigado por entrar em contato com a gente.');
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.send(`O que você me enviou foi: ${req.body.qualquercoisa}`);
 });
 
 app.listen(3000, () => {

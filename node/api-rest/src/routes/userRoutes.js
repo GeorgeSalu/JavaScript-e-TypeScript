d9920f10a29bea4e755cb4b8a7e4c83225957744
George Salu <george.salu10@gmail.com>
2020-11-29 15:42:26 -0300

    removendo comentarios

--- node/api-rest/src/routes/userRoutes.js
+++ node/api-rest/src/routes/userRoutes.js
@@ -5,11 +5,3 @@ const router = new Router();
 router.post('/', userController.store);
 
 export default router;
-
-/**
- * index -> lista todos os usuarios
- * store/create -> cria um novo usuario
- * delete -> apaga um usuario
- * show -> mostra um usuario
- * update -> atualiza um usuario
- */

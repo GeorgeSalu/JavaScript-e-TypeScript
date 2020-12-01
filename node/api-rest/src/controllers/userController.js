2b5ea88711ae69eb7067458a4fec916cbb40a361
George Salu <george.salu10@gmail.com>
2020-11-30 21:53:07 -0300

    crud de usuarios

--- node/api-rest/src/controllers/userController.js
+++ node/api-rest/src/controllers/userController.js
@@ -32,7 +32,7 @@ class UserController {
 
 	async update(req, res) {
 		try {
-			if(req.params.id) {
+			if(!req.params.id) {
 				return res.status(400).json({
 					errors: ['ID não enviado']
 				})
@@ -48,7 +48,33 @@ class UserController {
 			const novosDados = await	user.update(req.body);
 			return res.json(novosDados);
 		} catch (error) {
+			return res.status(400).json({
+				errors: e.errors.map(err => err.message)
+			})
+		}
+	}
+
+	async delete(req, res) {
+		try {
+			if(!req.params.id) {
+				return res.status(400).json({
+					errors: ['ID não enviado']
+				})
+			}
+			const user = await User.findByPk(req.params.id);
+
+			if(!user) {
+				return res.status(400).json({
+					errors: ['usuario não existe']
+				})
+			}
+
+			await	user.destroy();
 			return res.json(null);
+		} catch (error) {
+			return res.status(400).json({
+				errors: e.errors.map(err => err.message)
+			})
 		}
 	}
 }

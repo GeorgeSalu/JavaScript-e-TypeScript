747fb080204fae92f9e01bd13b2ba1f2c4d4307c
George Salu <george.salu10@gmail.com>
2020-12-01 11:27:45 -0300

    gerando o jwt do usuario

--- node/api-rest/src/controllers/tokenController.js
+++ node/api-rest/src/controllers/tokenController.js
@@ -1,8 +1,25 @@
 import Aluno from '../models/Aluno';
+import User from '../models/User';
 
 class TokenController {
 	async	store(req, res) {
-		res.json('ok');
+		const {email = '', password = ''} = req.body;
+
+		if(!email || !password) {
+			return res.status(401).json({
+				erros: ['credenciais invalidadas']
+			})
+		}
+
+		const user = await User.findOne({ where: { email } })
+
+		if(!user) {
+			return res.status(401).json({
+				erros: ['usuario não existe']
+			})
+		}
+
+		return res.json(user);
 	}
 }
 

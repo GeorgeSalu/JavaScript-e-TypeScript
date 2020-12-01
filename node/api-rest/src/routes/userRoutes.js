a51c6ab26f49927fded1f32a7500dddf5b92ab36
George Salu <george.salu10@gmail.com>
2020-12-01 17:09:26 -0300

    correções de segunranca para usuarios

--- node/api-rest/src/routes/userRoutes.js
+++ node/api-rest/src/routes/userRoutes.js
@@ -4,10 +4,11 @@ import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 
+router.get('/', loginRequired, userController.index); // lista usuarios
+router.get('/:id', userController.show); // lsiat usuario
+
 router.post('/', userController.store);
-router.get('/', loginRequired, userController.index);
-router.get('/:id', userController.show);
-router.put('/:id', userController.update);
-router.delete('/:id', userController.delete);
+router.put('/', loginRequired, userController.update);
+router.delete('/', loginRequired,  userController.delete);
 
 export default router;

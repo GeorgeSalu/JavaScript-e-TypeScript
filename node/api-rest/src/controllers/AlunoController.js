1795cbce3ef8faff11dffe8e722fb70e292f3e8e
George Salu <george.salu10@gmail.com>
2020-12-01 23:03:11 -0300

    crud de aluno

--- node/api-rest/src/controllers/AlunoController.js
+++ node/api-rest/src/controllers/AlunoController.js
@@ -5,6 +5,44 @@ class AlunoController {
 		const alunos = await Aluno.findAll();
 		res.json(alunos);
 	}
+
+	async store(req, res) {
+
+	}
+
+	async show(req, res) {
+		try {
+			const {id} = req.params;
+
+			if(!id) {
+				return res.status(400).json({
+					errors: ['faltando id']
+				})
+			}
+
+			const aluno = await Aluno.findByPk(id);
+
+			if(!aluno) {
+				return res.status(400).json({
+					errors: ['aluno nao existe']
+				})
+			}
+
+			return res.json(aluno);
+		} catch (e) {
+			return res.status(400).json({
+				errors: e.errors.map((err) => err.message)
+			})
+		}
+	}
+
+	async delete(req, res) {
+
+	}
+
+	async update(req, res) {
+
+	}
 }
 
 export default new AlunoController();
--- node/api-rest/src/routes/alunoRoutes.js
+++ node/api-rest/src/routes/alunoRoutes.js
@@ -3,5 +3,9 @@ import alunoController from '../controllers/AlunoController';
 const router = new Router();
 
 router.get('/', alunoController.index);
+router.post('/', alunoController.store);
+router.put('/:id', alunoController.update);
+router.get('/:id', alunoController.show);
+router.delete('/:id', alunoController.delete);
 
 export default router;

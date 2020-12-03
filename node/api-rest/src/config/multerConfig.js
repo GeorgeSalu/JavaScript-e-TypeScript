f28f48b7150739cd53789766e49bb013e1e5bf4f
George Salu <george.salu10@gmail.com>
2020-12-03 12:29:53 -0300

    corrigindo msg de erro

--- node/api-rest/src/config/multerConfig.js
+++ node/api-rest/src/config/multerConfig.js
@@ -6,7 +6,7 @@ const aleatorio = () => Math.floor(Math.random() * 10000 + 10000);
 export default {
   fileFilter: (req, file, cb) => {
     if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg') {
-      return cb(new multer.MulterError('Arquivo precisa ser PNG ou JPG.'));
+      return cb(new multer.MulterError('Arquivo precisa ser png ou jpg.'));
     }
 
     return cb(null, true);

84a64134957435673acaa548d299a6f78442bead
George Salu <george.salu10@gmail.com>
2020-12-06 14:00:18 -0300

    editando tarefas

--- react/listatarefas/src/components/Main.js
+++ react/listatarefas/src/components/Main.js
@@ -6,21 +6,33 @@ export default class Main extends Component {
 
   state = {
     novaTarefa: '',
-    tarefas: []
+    tarefas: [],
+    index: -1
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
-    const {tarefas} = this.state;
+    const {tarefas, index} = this.state;
     let {novaTarefa} = this.state;
     novaTarefa = novaTarefa.trim();
 
     if(tarefas.indexOf(novaTarefa) !== -1) return;
     
     const novasTarefas = [...tarefas];
-    this.setState({
-      tarefas: [...novasTarefas, novaTarefa]
-    })
+
+    if(index === -1) {
+      this.setState({
+        tarefas: [...novasTarefas, novaTarefa],
+        novaTarefa: '',
+      })
+    } else {
+      novasTarefas[index] = novaTarefa;
+
+      this.setState({
+        tarefas: [...novasTarefas],
+        index: -1
+      })
+    }
   }
 
   handleChange = (e) => {
@@ -30,7 +42,12 @@ export default class Main extends Component {
   }
 
   handleEdit = (e, index) => {
+    const {tarefas} = this.state;
 
+    this.setState({
+      index,
+      novaTarefa: tarefas[index],
+    })
   }
 
   handleDelete = (e, index) => {

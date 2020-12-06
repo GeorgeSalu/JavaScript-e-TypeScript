ac13e3ecc683d22fd27d421c25af42302f523e1c
George Salu <george.salu10@gmail.com>
2020-12-06 12:18:15 -0300

    adicionando tarefas

--- react/listatarefas/src/components/Main.js
+++ react/listatarefas/src/components/Main.js
@@ -6,12 +6,22 @@ export default class Main extends Component {
 
   state = {
     novaTarefa: '',
-    tarefas: [
-      'fazer cafe',
-      'beber agua',
-      'estudar'
-    ]
+    tarefas: []
   };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const {tarefas} = this.state;
+    let {novaTarefa} = this.state;
+    novaTarefa = novaTarefa.trim();
+
+    if(tarefas.indexOf(novaTarefa) != -1) return;
+    
+    const novasTarefas = [...tarefas];
+    this.setState({
+      tarefas: [...novasTarefas, novaTarefa]
+    })
+  }
 
   handleChange = (e) => {
     this.setState({
@@ -25,7 +35,7 @@ export default class Main extends Component {
       <div className="main">
         <h1>Lista de tarefas</h1>
         
-        <form action="#" className="form">
+        <form onSubmit={this.handleSubmit} action="#" className="form">
           <input 
             type="text" 
             onChange={this.handleChange} 

4cf51701924801f29307fb5f41762cd5ba25823d
George Salu <george.salu10@gmail.com>
2020-12-07 14:23:31 -0300

    buscando dados da api

--- react/consumirapi/src/pages/Alunos/index.js
+++ react/consumirapi/src/pages/Alunos/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container } from '../../styles/GlobalStyles';
+import { AlunoContainer } from './styled';
 import axios from '../../services/axios';
 
 export default function Alunos() {
-  React.useEffect(() => {
+  const [alunos, setAlunos] = useState([]);
+
+  useEffect(() => {
     async function getData() {
-      const response = await axios.get('/alunos/');
-      console.log(response.data);
+      const response = await axios.get('/alunos');
+      setAlunos(response.data);
     }
 
     getData();
@@ -15,6 +18,14 @@ export default function Alunos() {
   return (
     <Container>
       <h1>Alunos</h1>
+
+      <AlunoContainer>
+        {alunos.map((aluno) => (
+          <div key={String(aluno.id)}>
+            <img src={aluno.Fotos[0].url} alt="" />
+          </div>
+        ))}
+      </AlunoContainer>
     </Container>
   );
 }

7fb9954d89b443945fcd01c2316264d4f1048b6d
George Salu <george.salu10@gmail.com>
2020-12-07 16:45:44 -0300

    cadastrando novos usuarios na aplicacao

--- react/consumirapi/src/pages/Register/index.js
+++ react/consumirapi/src/pages/Register/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { toast } from 'react-toastify';
 import { Container } from '../../styles/GlobalStyles';
 import { Form } from './styled';
 
@@ -7,10 +8,24 @@ export default function Register() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  function handlesubmit(e) {
+    e.preventDefault();
+    let formErrors = false;
+
+    if (nome.length < 3 || nome.length > 255) {
+      formErrors = true;
+      toast.error('nome deve ter entre 3 e 255 caractres');
+    }
+    if (password.length < 6 || password.length > 50) {
+      formErrors = true;
+      toast.error('senha deve ter entre 6 e 50 caracteres');
+    }
+  }
+
   return (
     <Container>
       <h1>Crie sua conta.</h1>
-      <Form>
+      <Form onSubmit={handlesubmit}>
         <label htmlFor="nome">
           Nome:
           <input
@@ -38,6 +53,8 @@ export default function Register() {
             onChange={(e) => setPassword(e.target.value)}
           />
         </label>
+
+        <button type="submit">Criar minha conta</button>
       </Form>
     </Container>
   );

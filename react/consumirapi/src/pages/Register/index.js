083b9f278ced6b51c4aff3f20d66b99a50a18574
George Salu <george.salu10@gmail.com>
2020-12-07 17:33:30 -0300

    casdastrando novos usuarios na aplicação

--- react/consumirapi/src/pages/Register/index.js
+++ react/consumirapi/src/pages/Register/index.js
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
 import { toast } from 'react-toastify';
+import { isEmail } from 'validator';
+import { get } from 'lodash';
 import { Container } from '../../styles/GlobalStyles';
 import { Form } from './styled';
+import axios from '../../services/axios';
 
 export default function Register() {
   const [nome, setNome] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  function handlesubmit(e) {
+  async function handlesubmit(e) {
     e.preventDefault();
     let formErrors = false;
 
@@ -16,9 +19,28 @@ export default function Register() {
       formErrors = true;
       toast.error('nome deve ter entre 3 e 255 caractres');
     }
+    if (!isEmail(email)) {
+      formErrors = true;
+      toast.error('email invalido');
+    }
     if (password.length < 6 || password.length > 50) {
       formErrors = true;
       toast.error('senha deve ter entre 6 e 50 caracteres');
+    }
+
+    if (formErrors) return;
+
+    try {
+      const response = axios.post('/users/', {
+        nome,
+        password,
+        email,
+      });
+    } catch (e) {
+      const status = get(e, 'response.status', 0);
+      const errors = get(e, 'response.data.errors', []);
+
+      errors.map((error) => toast.error(error));
     }
   }
 

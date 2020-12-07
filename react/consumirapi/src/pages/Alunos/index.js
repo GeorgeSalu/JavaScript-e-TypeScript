efc20ca77fefc66a7b3a71a093feab462ec5a6d1
George Salu <george.salu10@gmail.com>
2020-12-07 15:31:59 -0300

    buscando dados da api

--- react/consumirapi/src/pages/Alunos/index.js
+++ react/consumirapi/src/pages/Alunos/index.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import { get } from 'lodash';
+import { Link } from 'react-router-dom';
+import { FaUserCircle, FaEdit, FaWindowClose } from 'react-icons/fa';
+
 import { Container } from '../../styles/GlobalStyles';
-import { AlunoContainer } from './styled';
+import { AlunoContainer, ProfilePicture } from './styled';
 import axios from '../../services/axios';
 
 export default function Alunos() {
@@ -22,7 +26,24 @@ export default function Alunos() {
       <AlunoContainer>
         {alunos.map((aluno) => (
           <div key={String(aluno.id)}>
-            <img src={aluno.Fotos[0].url} alt="" />
+            <ProfilePicture>
+              {get(aluno, 'Fotos[0].url', false) ? (
+                <img src={aluno.Fotos[0].url} alt="" />
+              ) : (
+                <FaUserCircle size={36} />
+              )}
+            </ProfilePicture>
+
+            <span>{aluno.nome}</span>
+            <span>{aluno.email}</span>
+
+            <Link to={`/aluno/${aluno.id}/edit`}>
+              <FaEdit size={16} />
+            </Link>
+
+            <Link to={`/aluno/${aluno.id}/delete`}>
+              <FaWindowClose size={16} />
+            </Link>
           </div>
         ))}
       </AlunoContainer>

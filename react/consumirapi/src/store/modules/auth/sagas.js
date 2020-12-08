39b967dacd123e718f6e00878e2a1aad84a82a10
George Salu <george.salu10@gmail.com>
2020-12-08 10:41:16 -0300

    login #2

--- react/consumirapi/src/store/modules/auth/sagas.js
+++ react/consumirapi/src/store/modules/auth/sagas.js
@@ -1,6 +1,23 @@
-import { all, takeLatest } from 'redux-saga/effects';
+import { all, call, put, takeLatest } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 import * as types from '../types';
+import axios from '../../../services/axios';
+import * as actions from './actions';
+import history from '../../../services/history';
 
-function* loginRequest({ payload }) {}
+function* loginRequest({ payload }) {
+  try {
+    const response = yield call(axios.post, '/tokens', payload);
+
+    yield put(actions.loginSuccess({ ...response.data }));
+    toast.success('voce fez login');
+    axios.defaults.headers.Authorization = `Bearer ${response.data.token}`;
+    history.push(payload.prevPath);
+  } catch (e) {
+    toast.error('usuario ou senha invalidos');
+
+    yield put(actions.loginFailure);
+  }
+}
 
 export default all([takeLatest(types.LOGIN_REQUEST, loginRequest)]);

5fc1a05d69ff9d5a4d39564bdc0e6bd6bd81bc36
George Salu <george.salu10@gmail.com>
2020-12-08 22:22:50 -0300

    sintaxe de types annotations no typescript

--- cursotypescript/src/type-annotation/type-annotation.ts
+++ cursotypescript/src/type-annotation/type-annotation.ts
@@ -4,3 +4,20 @@ let nome: string = 'luiz'
 let idade: number = 30;
 let adulto: boolean = true;
 let simbolo: symbol = Symbol('quaquer-simbol');
+
+// arrays
+let arrayDeNumeros: Array<number> = [1,2,3,4];
+let arrayDeStrings: Array<string> = ['a','b', 'c'];
+
+//objetos
+let pessoa: {nome: string, idade: number, adulto?: boolean} = {
+  idade: 30,
+  nome: 'luiz'
+}
+
+//funcoes
+function soma(x: number, y: number): number {
+  return x + y
+}
+
+const soma2: (x: number, y: number) => number = (x, y) => x + y

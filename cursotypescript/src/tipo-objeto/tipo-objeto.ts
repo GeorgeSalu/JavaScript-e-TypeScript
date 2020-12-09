19e0b54ee9618f39b19dd603ea522d4b2d2f3ad3
George Salu <george.salu10@gmail.com>
2020-12-09 15:09:02 -0300

    tipo objeto

---
+++ cursotypescript/src/tipo-objeto/tipo-objeto.ts
@@ -0,0 +1,11 @@
+const objetoA: {
+  chaveA: string;
+  chaveB: string;
+  chaveC?: string;
+} = {
+  chaveA: 'Valor A',
+  chaveB: 'Valor B',
+};
+
+objetoA.chaveA = 'Outo valor';
+objetoA.chaveB = 'outra chave';

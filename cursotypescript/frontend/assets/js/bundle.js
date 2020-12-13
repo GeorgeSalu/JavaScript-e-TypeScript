6cc57dbed0c185d79c5c532931b062da69f8dfb6
George Salu <george.salu10@gmail.com>
2020-12-13 10:45:49 -0300

    validando um formulario

--- cursotypescript/frontend/assets/js/bundle.js
+++ cursotypescript/frontend/assets/js/bundle.js
@@ -95,7 +95,54 @@
 
 "use strict";
 
-console.log('mod');
+var SHOW_ERROR_MESSAGES = 'show-error-message';
+var form = document.querySelector('.form');
+var username = document.querySelector('.username');
+var email = document.querySelector('.email');
+var password = document.querySelector('.password');
+var password2 = document.querySelector('.password2');
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    hideErrorMessages(this);
+    checkForEmptyFields(username, email, password, password2);
+    checkEqualPasswords(password, password2);
+    if (shouldSendForm(this))
+        console.log('FORMULÁRIO ENVIADO');
+});
+function checkForEmptyFields() {
+    var inputs = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        inputs[_i] = arguments[_i];
+    }
+    inputs.forEach(function (input) {
+        if (!input.value)
+            showErrorMessage(input, 'Campo não pode ficar vazio');
+    });
+}
+function checkEqualPasswords(password, password2) {
+    if (password.value !== password2.value) {
+        showErrorMessage(password, 'Senhas não batem');
+        showErrorMessage(password2, 'Senhas não batem');
+    }
+}
+function hideErrorMessages(form) {
+    form
+        .querySelectorAll('.' + SHOW_ERROR_MESSAGES)
+        .forEach(function (item) { return item.classList.remove(SHOW_ERROR_MESSAGES); });
+}
+function showErrorMessage(input, msg) {
+    var formFields = input.parentElement;
+    var errorMessage = formFields.querySelector('.error-message');
+    errorMessage.innerText = msg;
+    formFields.classList.add(SHOW_ERROR_MESSAGES);
+}
+function shouldSendForm(form) {
+    var send = true;
+    form
+        .querySelectorAll('.' + SHOW_ERROR_MESSAGES)
+        .forEach(function () { return (send = false); });
+    return send;
+}
 
 
 /***/ }),

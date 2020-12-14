efa13a1145fda16ce8bf4f8c9a05bf0be4fa8b58
George Salu <george.salu10@gmail.com>
2020-12-13 21:38:27 -0300

    heranca

--- cursotypescript/src/classeEInterfaces/heranca.ts
+++ cursotypescript/src/classeEInterfaces/heranca.ts
@@ -19,5 +19,21 @@ export class Pessoa {
   }
 }
 
-export class Aluno extends Pessoa {}
-export class Cliente extends Pessoa {}
+export class Aluno extends Pessoa {
+  getNomeCompleto(): string {
+    return 'isso vem de aluno ' + this.nome + ' ' + this.sobrenome;
+  }
+}
+export class Cliente extends Pessoa {
+  getNomeCompleto(): string {
+    return 'isso vem de cliente ' + this.nome + ' ' + this.sobrenome;
+  }
+}
+
+const pessoa = new Pessoa('luiz', 'miranda', 30, '000.000.000-00');
+const aluno = new Aluno('luiz', 'miranda', 30, '000.000.000-00');
+const cliente = new Cliente('luiz', 'miranda', 30, '000.000.000-00');
+
+console.log(pessoa.getNomeCompleto());
+console.log(aluno.getNomeCompleto());
+console.log(cliente.getNomeCompleto());

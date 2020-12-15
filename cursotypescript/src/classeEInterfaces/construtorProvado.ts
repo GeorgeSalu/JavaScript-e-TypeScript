97366fe1f235f18ab1d6e647c5b08510ab0a9c89
George Salu <george.salu10@gmail.com>
2020-12-15 10:15:33 -0300

    construtor provador e singleton

--- cursotypescript/src/classeEInterfaces/construtorProvado.ts
+++ cursotypescript/src/classeEInterfaces/construtorProvado.ts
@@ -1,5 +1,7 @@
 export class Database {
-  constructor(
+  private static database: Database;
+
+  private constructor(
     private host: string,
     private user: string,
     private password: string,
@@ -8,7 +10,18 @@ export class Database {
   connect(): void {
     console.log(`conectado: ${this.host}, ${this.user}, ${this.password}`);
   }
+
+  static getDatabase(host: string, user: string, password: string): Database {
+    if (Database.database) return Database.database;
+    Database.database = new Database(host, user, password);
+    return Database.database;
+  }
 }
 
-const database = new Database('localhost', 'root', '123456');
+const database = Database.getDatabase('localhost', 'root', '123456');
 database.connect();
+
+const database2 = Database.getDatabase('localhost', 'root', '123456');
+database.connect();
+
+console.log(database === database2);

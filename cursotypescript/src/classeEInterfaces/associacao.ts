4b8b3b06f16fe72ad2cb1468f266f91a5120322f
George Salu <george.salu10@gmail.com>
2020-12-15 11:37:44 -0300

    associacao entre classes

--- cursotypescript/src/classeEInterfaces/associacao.ts
+++ cursotypescript/src/classeEInterfaces/associacao.ts
@@ -2,6 +2,26 @@ export class Escritor {
   private _ferramenta: Ferramenta | null = null;
 
   constructor(private _nome: string) {}
+
+  get nome(): string {
+    return this._nome;
+  }
+
+  set ferramenta(ferramenta: Ferramenta | null) {
+    this._ferramenta = ferramenta;
+  }
+
+  get ferramenta(): Ferramenta | null {
+    return this._ferramenta;
+  }
+
+  escrever(): void {
+    if (this.ferramenta === null) {
+      console.log('Não posso escrever sem ferramenta...');
+      return;
+    }
+    this.ferramenta.escrever();
+  }
 }
 
 export abstract class Ferramenta {
@@ -9,6 +29,31 @@ export abstract class Ferramenta {
   abstract escrever(): void;
 
   get nome(): string {
-    return this.nome;
+    return this._nome;
   }
 }
+
+export class Caneta extends Ferramenta {
+  escrever(): void {
+    console.log(`${this.nome} está escrevendo...`);
+  }
+}
+
+export class MaquinaEscrever extends Ferramenta {
+  escrever(): void {
+    console.log(`${this.nome} está digitando...`);
+  }
+}
+
+const escritor = new Escritor('Luiz');
+const caneta = new Caneta('Bic');
+const maquinaEscrever = new MaquinaEscrever('Máquina');
+
+// console.log(escritor.nome);
+// console.log(caneta.nome);
+// console.log(maquinaEscrever.nome);
+
+escritor.ferramenta = caneta;
+escritor.ferramenta = maquinaEscrever;
+escritor.ferramenta = null;
+escritor.escrever();

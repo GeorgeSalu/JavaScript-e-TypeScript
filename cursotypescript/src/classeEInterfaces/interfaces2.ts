d542cf3acc7ee1b10a500e3729175a5c6cca272e
George Salu <george.salu10@gmail.com>
2020-12-15 15:15:41 -0300

    declaration merging em interfaces

---
+++ cursotypescript/src/classeEInterfaces/interfaces2.ts
@@ -0,0 +1,17 @@
+// Declaration merging
+interface Pessoa {
+  nome: string;
+}
+
+interface Pessoa {
+  readonly sobrenome: string;
+  readonly enderecos: readonly string[];
+  idade?: number;
+}
+
+const pessoa2: Pessoa = {
+  nome: 'Luiz',
+  sobrenome: 'Miranda',
+  enderecos: ['Av. Brasil'],
+  idade: 30,
+};

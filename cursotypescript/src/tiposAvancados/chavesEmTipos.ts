be29c7627396cf03b2bfeee04ee0ba6a3db44805
George Salu <george.salu10@gmail.com>
2020-12-15 21:16:52 -0300

    usando chaves com tipos

--- cursotypescript/src/tiposAvancados/chavesEmTipos.ts
+++ cursotypescript/src/tiposAvancados/chavesEmTipos.ts
@@ -4,14 +4,14 @@ type Veiculo = {
 };
 
 type Car = {
-  brand: string;
-  year: string;
+  brand: Veiculo['marca'];
+  year: Veiculo['ano'];
   name: string;
 };
 
 const carro: Car = {
   brand: 'Ford',
-  year: '2020',
+  year: 2020,
   name: 'Nome',
 };
 

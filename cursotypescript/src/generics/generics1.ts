fc59a3f0e620c7d5c2ec2e2d02516d9aedf026f5
George Salu <george.salu10@gmail.com>
2020-12-16 13:58:12 -0300

    introdução aos tipos genericos

--- cursotypescript/src/generics/generics1.ts
+++ cursotypescript/src/generics/generics1.ts
@@ -1,13 +1,6 @@
-type FilterCallback = (
-  value: unknown,
-  index?: number,
-  array?: unknown[],
-) => boolean;
+type FilterCallback<U> = (value: U, index?: number, array?: U[]) => boolean;
 
-export function meuFilter(
-  array: unknown[],
-  callbackfn: FilterCallback,
-): unknown {
+export function meuFilter<T>(array: T[], callbackfn: FilterCallback<T>): T[] {
   const novoArray = [];
 
   for (let i = 0; i < array.length; i++) {
@@ -15,15 +8,14 @@ export function meuFilter(
       novoArray.push(array[i]);
     }
   }
+
   return novoArray;
 }
 
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
 const arrayFiltradoOriginal = array.filter((value) => value < 5);
 console.log(arrayFiltradoOriginal);
 
-const arrayFiltrado = meuFilter(array, (value) => {
-  if (typeof value === 'number') return value < 5;
-  return false;
-});
+const arrayFiltrado = meuFilter(array, (value) => value < 5);
 console.log(arrayFiltrado);

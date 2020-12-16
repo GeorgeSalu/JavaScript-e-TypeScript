ae274cf22c47a5d5776a753126a3410eb1e6c1d3
George Salu <george.salu10@gmail.com>
2020-12-16 15:30:31 -0300

    generic com interface e typealias

--- cursotypescript/src/generics/generics3.ts
+++ cursotypescript/src/generics/generics3.ts
@@ -1,13 +1,34 @@
-interface PessoaProtocolo<T, U> {
+interface PessoaProtocolo<T = string, U = number> {
   nome: T;
   sobrenome: T;
   idade: U;
 }
 
-const aluno1: PessoaProtocolo<string, number> = {
-  nome: 'luiz',
-  sobrenome: 'miranda',
+type PessoaProtocolo2<T = string, U = number> = {
+  nome: T;
+  sobrenome: T;
+  idade: U;
+};
+
+const aluno1: PessoaProtocolo2<string, number> = {
+  nome: 'Luiz',
+  sobrenome: 'Miranda',
   idade: 30,
 };
 
-console.log(aluno1);
+const aluno2: PessoaProtocolo<number, number> = {
+  nome: 123,
+  sobrenome: 456,
+  idade: 30,
+};
+
+const aluno3: PessoaProtocolo2 = {
+  nome: 'Luiz',
+  sobrenome: 'Miranda',
+  idade: 30,
+};
+
+console.log(aluno1, aluno2, aluno3);
+
+// Module mode
+export default 1;

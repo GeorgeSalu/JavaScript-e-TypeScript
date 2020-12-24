1458b3bcda2d7cfed38dff84384ebb7fe244d1c3
George Salu <george.salu10@gmail.com>
2020-12-24 16:11:45 -0300

    ocp - principio do aberto fechado

---
+++ typescriptsolid/src/ocp/main.ts
@@ -0,0 +1,19 @@
+import { Messaging } from './service/messaging';
+import { Order } from './entities/order';
+import { Persistence } from './service/persistence';
+import { Product } from './entities/product';
+import { ShoppingCart } from './entities/shopping-cart';
+
+const shoppingCart = new ShoppingCart();
+const messaging = new Messaging();
+const persistence = new Persistence();
+const order = new Order(shoppingCart, messaging, persistence);
+shoppingCart.addItem(new Product('camisa', 49.91));
+shoppingCart.addItem(new Product('caderno', 9.9123));
+shoppingCart.addItem(new Product('lapis', 1.59));
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDicount());
+order.checkout();
+console.log(order.orderStatus);

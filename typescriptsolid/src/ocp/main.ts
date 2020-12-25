3a7826201ede1455d2239eed60ff7c28ece98c7e
George Salu <george.salu10@gmail.com>
2020-12-24 21:14:32 -0300

    fix lint

--- typescriptsolid/src/ocp/main.ts
+++ typescriptsolid/src/ocp/main.ts
@@ -8,6 +8,7 @@ const shoppingCart = new ShoppingCart();
 const messaging = new Messaging();
 const persistence = new Persistence();
 const order = new Order(shoppingCart, messaging, persistence);
+
 shoppingCart.addItem(new Product('camisa', 49.91));
 shoppingCart.addItem(new Product('caderno', 9.9123));
 shoppingCart.addItem(new Product('lapis', 1.59));

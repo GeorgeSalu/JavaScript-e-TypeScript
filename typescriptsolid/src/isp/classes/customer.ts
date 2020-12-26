6b22d856ac723e67fecc123ab382730e8476ee68
George Salu <george.salu10@gmail.com>
2020-12-26 13:52:08 -0300

    isp

---
+++ typescriptsolid/src/isp/classes/customer.ts
@@ -0,0 +1,13 @@
+import { CustomerProtocol } from './interfaces/customer-protocol';
+export class IndividualCustomer implements CustomerProtocol {
+  firstName: string;
+  lastName: string;
+  cpf: string;
+  cnpj: string;
+}
+export class EnterpriseCustomer implements CustomerProtocol
+  firstName: string;
+  lastName: string;
+  cpf: string;
+  cnpj: string;
+{}

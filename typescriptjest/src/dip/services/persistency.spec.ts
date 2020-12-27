4576880669f754c17f362cbb4f3be2f7d6d63fe9
George Salu <george.salu10@gmail.com>
2020-12-26 21:57:15 -0300

    introdução ao jest

---
+++ typescriptjest/src/dip/services/persistency.spec.ts
@@ -0,0 +1,9 @@
+it('descrição do teste (it)', () => {
+  const number = 1;
+  expect(number).toBe(1);
+});
+
+test('descrição do teste (it)', () => {
+  const nome = 'luiz';
+  expect(nome).toBe('luiz');
+});

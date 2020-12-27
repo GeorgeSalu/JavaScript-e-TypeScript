65fd76b4ee4ae92f0bccc983ffdad8a09cd5de77
George Salu <george.salu10@gmail.com>
2020-12-26 22:39:14 -0300

    primeiro teste cin spyOn

--- typescriptjest/src/services/persistency.spec.ts
+++ typescriptjest/src/services/persistency.spec.ts
@@ -1,9 +1,27 @@
-it('descrição do teste (it)', () => {
-  const number = 1;
-  expect(number).toBe(1);
-});
+import { Persistency } from './persistency';
 
-test('descrição do teste (it)', () => {
-  const nome = 'luiz';
-  expect(nome).toBe('luiz');
+describe('persistency', () => {
+  afterEach(() => jest.clearAllMocks());
+
+  it('should return undefined', () => {
+    // system under test
+    const sut = new Persistency();
+    expect(sut.saveOrder()).toBeUndefined();
+  });
+
+  it('should call console.log once', () => {
+    // system under test
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call console.log with "Pedido salvo com sucesso..."', () => {
+    // system under test
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledWith('Pedido salvo com sucesso...');
+  });
 });

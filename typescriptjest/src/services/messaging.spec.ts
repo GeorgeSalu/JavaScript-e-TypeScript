ada65767ef09eb47e3ff6f225bcefc4c58c846e8
George Salu <george.salu10@gmail.com>
2020-12-26 22:55:42 -0300

    testando messaging.ts

---
+++ typescriptjest/src/services/messaging.spec.ts
@@ -0,0 +1,31 @@
+import { Messaging } from './messaging';
+
+const createSut = () => {
+  return new Messaging();
+};
+
+describe('messaging', () => {
+  afterEach(() => jest.clearAllMocks());
+
+  it('should return undefined', () => {
+    // system under test
+    const sut = createSut();
+    expect(sut.sendMessage('teste')).toBeUndefined();
+  });
+
+  it('should call console.log once', () => {
+    // system under test
+    const sut = createSut();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.sendMessage('teste');
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call console.log with "Mensagem enviada:"', () => {
+    // system under test
+    const sut = createSut();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.sendMessage('teste');
+    expect(consoleSpy).toHaveBeenCalledWith('Mensagem enviada:', 'teste');
+  });
+});

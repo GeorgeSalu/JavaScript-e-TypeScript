022c71f1a26b54752a16b4f5260ce9a2c20c798d
George Salu <george.salu10@gmail.com>
2020-12-28 13:11:15 -0300

    removendo texto de teste

--- pomodoro/src/App.tsx
+++ pomodoro/src/App.tsx
@@ -3,7 +3,6 @@ import { PomodoroTimer } from './components/pomodoro-timer';
 function App(): JSX.Element {
   return (
     <div className="container">
-      <h1>Ola mundo</h1>
       <PomodoroTimer
         pomodoroTime={3600}
         shortRestTime={300}

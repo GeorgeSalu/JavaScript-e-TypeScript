66d32c43394ac155394313119360d9c600dee398
George Salu <george.salu10@gmail.com>
2020-12-28 11:31:39 -0300

    incluindo props no componente pomodoto-timer

--- pomodoro/src/App.tsx
+++ pomodoro/src/App.tsx
@@ -3,9 +3,14 @@ import { PomodoroTimer } from './components/pomodoro-timer';
 
 function App(): JSX.Element {
   return (
-    <div className="App">
+    <div className="container">
       <h1>Ola mundo</h1>
-      <PomodoroTimer defaultPomodoroTime={3600} />
+      <PomodoroTimer
+        pomodoroTime={3600}
+        shortRestTime={300}
+        longRestTime={900}
+        cycles={4}
+      />
     </div>
   );
 }

a2f0d06e9bcff8b65138734c82f1e6004d9916ee
George Salu <george.salu10@gmail.com>
2020-12-30 15:19:10 -0300

    singleton classic

--- design-patterns/src/criacionais/singleton/module_a.ts
+++ design-patterns/src/criacionais/singleton/module_a.ts
@@ -1,7 +1,9 @@
 import { MyDatabaseClassic } from './db/my-database-classic';
 
-const myDatabaseClassic = MyDatabaseClassic.getInstance();
+const myDatabaseClassic = MyDatabaseClassic.instance;
 myDatabaseClassic.add({ name: 'luiz', age: 30 });
 myDatabaseClassic.add({ name: 'mario', age: 40 });
 myDatabaseClassic.add({ name: 'eduardo', age: 20 });
 myDatabaseClassic.show();
+
+export { myDatabaseClassic };
--- design-patterns/src/criacionais/singleton/module_b.ts
+++ design-patterns/src/criacionais/singleton/module_b.ts
@@ -1,8 +1,10 @@
 import { MyDatabaseClassic } from './db/my-database-classic';
-import './module_a';
+import { myDatabaseClassic as myDatabaseClassicFromModuleA } from './module_a';
 
-const myDatabaseClassic = MyDatabaseClassic.getInstance();
+const myDatabaseClassic = MyDatabaseClassic.instance;
 myDatabaseClassic.add({ name: 'roberto', age: 30 });
 myDatabaseClassic.add({ name: 'joana', age: 40 });
 myDatabaseClassic.add({ name: 'luiza', age: 20 });
 myDatabaseClassic.show();
+
+console.log(myDatabaseClassic === myDatabaseClassicFromModuleA);

b6a44c5a97d36e08bf9e16802e3f4fea3c44b51b
George Salu <george.salu10@gmail.com>
2020-12-31 09:31:44 -0300

    singleton module

--- design-patterns/src/criacionais/singleton/module_b.ts
+++ design-patterns/src/criacionais/singleton/module_b.ts
@@ -1,10 +1,8 @@
-import { MyDatabaseClassic } from './db/my-database-classic';
-import { myDatabaseClassic as myDatabaseClassicFromModuleA } from './module_a';
+//import { MyDatabaseClassic } from './db/my-database-classic';
+import { MyDatabaseModule } from './db/my-database-module';
 
-const myDatabaseClassic = MyDatabaseClassic.instance;
+const myDatabaseClassic = MyDatabaseModule;
 myDatabaseClassic.add({ name: 'roberto', age: 30 });
 myDatabaseClassic.add({ name: 'joana', age: 40 });
 myDatabaseClassic.add({ name: 'luiza', age: 20 });
 myDatabaseClassic.show();
-
-console.log(myDatabaseClassic === myDatabaseClassicFromModuleA);
